ec79b1888e9140d8c9a2ea36bee6d7638b296461
Potato Mosh <mochimosh03@gmail.com>
2022-07-31 22:00:04 +0900

    Solved the problem of my day 1 frustration

--- Server-Setup/initial-setup.sh
+++ Server-Setup/initial-setup.sh
@@ -1,44 +1,71 @@
 #!/usr/bin/env bash
-
+export "MOCHIHOME=$(pwd)/test.sh" # PLEASE RE EDIT THIS EVN PLEASE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 export "NAME=$(whoami)"
-export BOT=MochiBot
-
+export BOT="\"MochiBot\""
+export LOOPEND=""
+clear
 printf "\nWelcome, %s\n" "$NAME!"
 printf "My name is %s\n" "$BOT"
 sleep 1
-printf "\nI will be helping you with your initial server set up!\n" 
+printf "\nWould you like to save me as a shell command? [y/N]\n"
+read -r COMMAND
+while [[ $LOOPEND == true ]]; 
+do
+if [[ "$COMMAND" == "y" ]]; then
+    alias mochibot='$MOCHIHOME'
+else 
+    $LOOPEND -eq true
+fi
+done
+
+
+printf "I will be helping you with your initial server set up!\n" 
 echo -e "What would you like to start with?"
-echo -e "   ---------------------------------
+echo -e "   =================================
 1. | User Management               |
-2. | Distro Update                 |
-3. | SSH Configs                   |
-4. | Custom Installation           |
+2. | Distro Management             |
+3. | SSH Management                |
+4. | Custom Management             |
 5. | Exit Menu                     |
-   ---------------------------------
+   =================================
 For help option try \"1 -h\" or \"2 -h\" and so on.
 "
 
 export one="User Management"
 export two="Distro Management"
-export three="SSH Configs"
+export three="SSH Management"
 export four="Custom Configuration"
 export five="Exit Menu"
 
 read -r USERINPUT
 
 # MochiBot Help Commands
+while [[ $LOOPEND == true ]]; do
 if [[ $USERINPUT == "1 -h" ]]; then
+
     echo -e "User Management:
-    - Adds a new user (Optional Inputs: Add Home Dir | Add Shell | Add User Discription)
-    - Add to a new group (this could be for the sudo group)
-    - Add or Change Password for the user"
+    - (1 -a) Adds a new user (Optional Inputs: Add Home Dir | Add Shell | Add User Discription)
+    - (1 -b) Add to a new group (this could be for the sudo group)
+    - (1 -c) Add or Change Password for the user"
 elif [[ $USERINPUT == "2 -h" ]]; then
     echo -e "Distro Update:
-    - Update the full distro
-    - (Optional) Upgrade the distro"
+    - (2 -a) Update the full distro
+    - (2 -b) Upgrade the distro"
+elif [[ $USERINPUT == "3 -h" ]]; then
+    echo -e "SSH Management
+    - (3 -a) Adds .ssh into:  $HOME 
+    - (3 -b) Vim into: $HOME/.ssh
+    - (3 -c) Configure sshd_config
+    - (3 -d) Restarts SSHD"
+elif [[ $USERINPUT == "4 -h" ]]; then
+    echo -e "WIP"
+else
+    $LOOPEND -eq true
 fi
+done
 # MochiBot telling the user his/her option
 if [[ $USERINPUT == "1" ]]; then
+
     echo -e "You have selected: $one\n"
 elif [[ $USERINPUT == "2" ]]; then
     echo -e "You have selected: $two\n"
@@ -48,14 +75,14 @@ elif [[ $USERINPUT == "4" ]]; then
     echo -e "You have selected: $four\n"
 elif [[ $USERINPUT == "5" ]]; then
     echo -e "You have selected: $five\n"
-elif [[ $USERINPUT -le "5" ]]; then
+elif [[ $USERINPUT -le "6" ]]; then
     echo "EERRROOORRRRRRRR"
     sleep 1
-    echo -e "Why you no select the numbersssssssss"
+    echo -e "You have selected something that I have not been program to do!"
     sleep 1
     echo -e "Plrease try again and select another option.."
+    exit 1
 fi
-
 sleep 1
 
 # This "if" statement is used for User Management Option
@@ -69,6 +96,7 @@ if [[ $USERINPUT  == "1" ]]; then
         if [[ $discription_answer == "y" ]]; then
             printf "What discription do you want to add to this user?"
             read -r discription
+        else return
         fi
     useradd -m -s /bin/bash -c "$discription" "$custom_user"
 fi
