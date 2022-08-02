11473da160ff7addd5a9f826d9db68a9704e5a5e
Potato Mosh <mochimosh03@gmail.com>
2022-08-02 22:11:55 +0900

    Almost Done With Menu 2

---
+++ Essentials/Menu-List/2-Distro-Management/2-menu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export A="Update the full distro"
+
+################
+################
+
+clear
+printf "I will be helping you with Distro Management!\n" 
+echo -e "What would you like to start with?"
+echo -e "   ============================
+a. | Update the full distro   |
+x. | Exit Menu                |
+   ============================\n"
+read -r SELECTION
--- Essentials/Menu-List/2-Distro-Management/Distro-Essentials/a-distro.sh
+++ Essentials/Menu-List/2-Distro-Management/Distro-Essentials/a-distro.sh
@@ -2,11 +2,13 @@
 export "LINE===================================================="
 echo "Would you like to update and upgrade your Distro? [Y/n]"
 read -r confirmation
+sudo echo
 
 if [[ $confirmation == "n" ]]; then
     echo "You have chosen not to upgrade your Distro."
 
 elif [[ $confirmation != "n" ]]; then
+
     echo -e "Getting ready to upgrade your Distro...\n"
     sleep 2
     sudo apt update && sudo apt upgrade -y
@@ -14,4 +16,14 @@ elif [[ $confirmation != "n" ]]; then
     echo -e "\n$LINE\nMochiBot is finishing some final touches...\n$LINE"
     sleep 1
     echo -e "\n$LINE\nMochiBot has successfully upgraded your Distro\n$LINE\n"
+
+else
+
+    echo "EERRROOORRRRRRRR"
+    sleep 1
+    echo -e "You have selected something that I have not been program to do!"
+    sleep 1
+    echo -e "Plrease try again and select another option..."
+    exit 1
+
 fi
