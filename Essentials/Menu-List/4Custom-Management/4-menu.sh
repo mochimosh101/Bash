cc10e83b01cce00474f3672e4acb2b92abbd224b
Potato Mosh <mochimosh03@gmail.com>
2022-08-02 12:03:22 +0900

    Updated 4 Menu Option C

--- Essentials/Menu-List/4Custom-Management/4-menu.sh
+++ Essentials/Menu-List/4Custom-Management/4-menu.sh
@@ -16,7 +16,33 @@ read -r SELECTION
 
 if [[ $SELECTION == "a" ]]; then
 
-    echo -e "$A\n"
+    echo -e "\n$A\n"
+
+elif [[ $SELECTION == "b" ]]; then
+
+    echo -e "\n$B\n"
+
+elif [[ $SELECTION == "c" ]]; then
+
+    echo -e "\n$C\n"
+    sleep 1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mochimosh101/MochiBot/main/Essentials/Menu-List/4Custom-Management/a-custom.sh
+)"
     
+
+elif [[ $SELECTION == "x" ]]; then
+
+    echo -e "\nExiting Custom Management Menu..."
+    sleep 1
+    exit 0
+
+else [[ $SELECTION != "a|b|c|x" ]]
+
+    echo "EERRROOORRRRRRRR"
+    sleep 1
+    echo -e "You have selected something that I have not been program to do!"
+    sleep 1
+    echo -e "Plrease try again and select another option.."
+    exit 1
 
 fi
