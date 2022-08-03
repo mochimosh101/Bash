57baa63e199509151e09f3d0e62bf581fbcc526a
Potato Mosh <mochimosh03@gmail.com>
2022-08-03 11:15:03 +0900

    Wrong Spelling of sleep

--- Essentials/Menu-List/4Custom-Management/Custom-Essentials/d-custom.sh
+++ Essentials/Menu-List/4Custom-Management/Custom-Essentials/d-custom.sh
@@ -26,7 +26,7 @@ if [[ "$DOCKER" != "n" ]]; then
     sudo apt-get remove docker docker-engine docker.io
     sleep 1
     sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
-    slep 1
+    sleep 1
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     sleep 1
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
