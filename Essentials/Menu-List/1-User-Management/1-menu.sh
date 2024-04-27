c0c3dda834b0b41a5f3db339646977d8d6338202
Jochim Edago <109833436+mochimosh101@users.noreply.github.com>
2024-04-27 00:17:03 -0600

    Update 1-menu.sh

--- Essentials/Menu-List/1-User-Management/1-menu.sh
+++ Essentials/Menu-List/1-User-Management/1-menu.sh
@@ -50,7 +50,8 @@ else
     echo -e "You have selected something that I have not been program to do!"
     sleep 1
     echo -e "Plrease try again and select another option..."
-    exit 1
+    sleep 1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mochimosh101/MochiBot/main/Menu.sh)"
 
 fi
    
